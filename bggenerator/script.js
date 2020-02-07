2ceab6c6310c649c2a1d6d991e703532baaf512c
wanlab-io <hello.wanlab@gmail.com>
2020-02-07 12:39:59 +0800

    inital project background generator website

---
+++ bggenerator/script.js
@@ -0,0 +1,21 @@
+// set form input
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient(){
+  body.style.background = "linear-gradient(to right, "
+  + color1.value
+  + ", "
+  + color2.value
+  + ")";
+
+  css.textContent = body.style.background + ";";
+}
+
+// set event lisentner for color 1
+color1.addEventListener("input", setGradient);
+
+// set event lisentner for color 2
+color2.addEventListener("input", setGradient);
